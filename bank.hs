a520a4d1256983d5b3b3fad458dcc902f48f527f
FranciscoThiesen <franciscogthiesen@gmail.com>
2019-12-06 17:54:27 -0300

    Adding credit, deposit and transfer

--- bank.hs
+++ bank.hs
@@ -10,18 +10,27 @@ type Account = TVar Integer
 -- Para compilar: ghc -dynamic bank.hs -package stm -o bank
 
 -- To-do
-    -- Adicionar transferencia entre contas ( idealmente )
     -- Adicionar concorrencia
     -- Ler de um arquivo .txt as operacoes a serem feitas!
 
+credit :: Integer -> Account -> STM ()
+credit amount account = do
+    current <- readTVar account
+    writeTVar account (current + amount)
 
+debit :: Integer -> Account -> STM ()
+debit amount account = do
+    current <- readTVar account
+    writeTVar account (current - amount )
 
--- Essa funcao so deve deixar sacar se existir saldo suficiente
-limitedWithdraw :: Account -> Integer -> STM ()
-limitedWithdraw acc amount = do
-    bal <- readTVar acc
-    check (amount <= 0 || amount <= bal)
-    writeTVar acc (bal - amount)
+transfer :: Integer -> Account -> Account -> STM ()
+transfer amount from to = do
+    fromVal <- readTVar from
+    if (fromVal - amount) >= 0
+        then do
+               debit amount from
+               credit amount to
+        else retry
 
 showAcc name acc = do
     bal <- atomically (readTVar acc)
@@ -29,8 +38,14 @@ showAcc name acc = do
     hPutStr stdout (show bal ++ "\n")
 
 main = do
-    acc <- atomically (newTVar 100)
-    showAcc "Conta corrente" acc
-    atomically (limitedWithdraw acc 50)
-    showAcc "Conta corrente" acc
-    
+    acc1 <- atomically (newTVar 100)
+    acc2 <- atomically (newTVar 100) 
+    showAcc "Conta1" acc1
+    showAcc "Conta2" acc2
+    atomically (debit 50 acc1)
+    showAcc "Conta1" acc1
+    showAcc "Conta2" acc2
+    atomically (transfer 50 acc2 acc1)
+    showAcc "Conta1" acc1
+    showAcc "Conta2" acc2
+
