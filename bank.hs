3daf3c4e1d3c8e27c5e6506b655917e1d2a1bcca
FranciscoThiesen <franciscogthiesen@gmail.com>
2019-12-10 00:05:42 -0300

    Adicionando codigo de transferencia sequencial, ilustrando o uso de orElse

--- bank.hs
+++ bank.hs
@@ -11,26 +11,30 @@ type Account = TVar Integer
 
 -- To-do
     -- Adicionar concorrencia
-    -- Ler de um arquivo .txt as operacoes a serem feitas!
+
 
 credit :: Integer -> Account -> STM ()
-credit amount account = do
-    current <- readTVar account
+credit amount account = do 
+    current <- readTVar account;
     writeTVar account (current + amount)
 
 debit :: Integer -> Account -> STM ()
 debit amount account = do
     current <- readTVar account
-    writeTVar account (current - amount )
+    check $ amount <= current
+    writeTVar account (current - amount)
+
 
 transfer :: Integer -> Account -> Account -> STM ()
 transfer amount from to = do
     fromVal <- readTVar from
-    if (fromVal - amount) >= 0
-        then do
-               debit amount from
-               credit amount to
-        else retry
+    check $ fromVal >= amount
+    debit amount from
+    credit amount to
+
+cascade_transfer :: Integer -> Account -> Account -> Account -> STM ()
+cascade_transfer amount from1 from2 to = do
+    orElse (transfer amount from1 to) $ (transfer amount from2 to)
 
 showAcc name acc = do
     bal <- atomically (readTVar acc)
@@ -38,14 +42,14 @@ showAcc name acc = do
     hPutStr stdout (show bal ++ "\n")
 
 main = do
-    acc1 <- atomically (newTVar 100)
-    acc2 <- atomically (newTVar 100) 
+    acc1 <- atomically (newTVar 40)
+    acc2 <- atomically (newTVar 50)
+    acc3 <- atomically (newTVar 0)
     showAcc "Conta1" acc1
     showAcc "Conta2" acc2
-    atomically (debit 50 acc1)
+    showAcc "Conta3" acc3
+    atomically $ cascade_transfer 49 acc1 acc2 acc3
     showAcc "Conta1" acc1
     showAcc "Conta2" acc2
-    atomically (transfer 50 acc2 acc1)
-    showAcc "Conta1" acc1
-    showAcc "Conta2" acc2
+    showAcc "Conta3" acc3
 
